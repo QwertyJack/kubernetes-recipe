5a3175a1fa3df55e7d3c8d20f6a8094bf341c8d0
jack <jack@example.com>
2019-09-30 01:35:09 +0800

    Fix some error

--- vagrant/config.rb
+++ vagrant/config.rb
@@ -11,3 +11,13 @@ $forwarded_ports = {}
 $subnet = "10.0.20"
 $os = "ubuntu1804"
 $network_plugin = "flannel"
+# Setting multi_networking to true will install Multus: https://github.com/intel/multus-cni
+$multi_networking = false
+# The first three nodes are etcd servers
+$etcd_instances = 3
+# The first two nodes are kube masters
+$kube_master_instances = 2
+# All nodes are kube nodes
+$kube_node_instances = $num_instances
+
+$inventory = "inventory/mycluster"
